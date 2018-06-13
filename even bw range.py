5d5a3352b347ac84c9ea46dd50e979d563bee4fd
vasudevann <34597062+vasudevann@users.noreply.github.com>
2018-06-13 20:31:07 +0530

    Update even bw range.py

--- even bw range.py
+++ even bw range.py
@@ -1,5 +1,5 @@
-m1=int(input())
-n1=int(input())
-evens = [str(x) for x in range(m1+1,n1) if x%2 == 0]
-odds = [str(x) for x in range(m1+1,n1) if x%2 == 1]
+m2=int(input())
+n2=int(input())
+evens = [str(x) for x in range(m2+1,n2) if x%2 == 0]
+odds = [str(x) for x in range(m2+1,n2) if x%2 == 1]
 print("\t" + "\t".join(evens))
