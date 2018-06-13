2da053eb4800a2ed0633a391f25b23c17383a60f
vasudevann <34597062+vasudevann@users.noreply.github.com>
2018-06-13 20:38:42 +0530

    Update prime bw range.py

--- prime bw range.py
+++ prime bw range.py
@@ -1,11 +1,11 @@
 w = int(input())
 e = int(input())
 
-for num in range(w,e+1):
+for tem in range(w,e+1):
    
-   if num > 1:
-       for i in range(2,num):
-           if (num % i) == 0:
+   if tem > 1:
+       for i in range(2,tem):
+           if (tem % i) == 0:
                break
        else:
-           print(num)
+           print(tem)
