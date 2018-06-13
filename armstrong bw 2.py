c6399401997ea7ddf8301e76dd650994164a680f
vasudevann <34597062+vasudevann@users.noreply.github.com>
2018-06-13 20:47:41 +0530

    Update armstrong bw 2.py

--- armstrong bw 2.py
+++ armstrong bw 2.py
@@ -1,6 +1,6 @@
-lower = int(input())
-upper = int(input())
-for numss in range(lower, upper):
+lower1 = int(input())
+upper1 = int(input())
+for numss in range(lower1, upper1):
    order = len(str(numss))
    sum = 0
    temp = numss
